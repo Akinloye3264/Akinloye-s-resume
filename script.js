8ce84ac4998a239273dfc83831ee179117bcccc5
Akinloye3264 <e.akinloye@alustudent.com>
2025-06-17 01:07:38 -0700

    still my summative

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleDarkMode() {
+  const isDark = document.body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+  }
+});
+
